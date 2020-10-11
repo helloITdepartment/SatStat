b5796557f9e3dcd805de975bdab7995f08990fb7
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-10-11 19:46:55 +0300

    Deleted a bunch of comment that was unnecessary

--- servertest.js
+++ servertest.js
@@ -177,7 +177,6 @@ function secondDropdownChanged() {
         qDropdown.disabled = false;
     }
     
-    
 }
 
 function requestData() {
@@ -211,50 +210,3 @@ function requestData() {
         }
     }
 }
-//console.log("1");
-//const path = require('path');
-//const dbPath = path.resolve(__dirname, 'Satellites.db');
-//
-//const sqlite3 = require('sqlite3').verbose();
-//let db = new sqlite3.Database(dbPath, (err) =>{
-//	if (err){
-//		return console.error(err.message);
-//    } else {
-//    }
-//});
-//
-//
-//let sql = "SELECT type, (strftime('%Y', launchdate)/10)*10 as decade, Count(launchdate) as howMany FROM launch GROUP  BY type, launchdate ORDER  BY launchdate;"
-//
-//db.all(sql, [], (err, rows) => {
-//    if (err) {
-//        throw err;
-//    }
-//
-//    var dict = {};
-//
-//    rows.forEach((row) => {
-//        let type = row["TYPE"];
-//        let howMany = row["howMany"];
-//        let decade = row["decade"];
-//
-//        let arr = type.split("/");
-//        arr.forEach((t) => {
-//            const k = [t, decade];
-//            if(dict[k]) {
-//                dict[k] = dict[k] + howMany
-//            } else {
-//                dict[k] = howMany
-//            }
-////            console.log(t + " [" + howMany + "] " + decade);
-//        });
-//    });
-//    for (var key in dict) {
-//        value = dict[key];
-//        keys = key.split(",");
-//        console.log(keys[0] + " [" + value + "] " + keys[1]);
-//    }
-//});
-//
-//db.close();
-
