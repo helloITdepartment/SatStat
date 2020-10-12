92b32be01100b012fd5729713f0995018ba42b69
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-10-12 15:10:49 +0300

    (Hopefully) finished the logic for producing a sensible sql query based on input, and temporarily have it only show lines where there's at least one thing going from A to B (going to replace this with a slider value)

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const sqlite3 = require('sqlite3').verbose();
 
 
 http.createServer(function(request, response) {
+    
     var u = request.url;
+    let sqlMap = {"decade" : "(strftime('%Y', launchdate)/10)*10" };
+    
     if(u.startsWith("/data")) {//Requesting database data
         
         let db = new sqlite3.Database(dbPath, (err) =>{
@@ -22,10 +25,31 @@ http.createServer(function(request, response) {
         let plural = fields[3];
         let q = fields[4];
         
-//        let sql = "SELECT (strftime('%Y', launchdate)/10)*10 as TYPE, (strftime('%Y', launchdate)/10)*10 as decade, Count((strftime('%Y', launchdate)/10)*10) as howMany FROM launch GROUP  BY decade ORDER  BY decade;"
-
+        let table = plural;
         
-        let sql = "SELECT type, (strftime('%Y', launchdate)/10)*10 as decade, Count(launchdate) as howMany FROM launch GROUP  BY type, launchdate ORDER  BY launchdate;"
+        if (table == "satellite") {
+            
+            if(singular == "type") {
+                singular = "purpose";
+            }
+            
+            if(q == "type"){
+                q = "purpose";
+            }
+            
+            if(q == "class"){
+                table = "satellite s inner join orbit o on s.satapogee=o.apogee AND s.satperigee=o.perigee AND s.satinclination=o.inclination AND s.satperiod=o.period";
+            }
+            
+        }else if(table == "launch" || table == "vehicle"){
+            
+            table = "launch inner join vehicle on launch.vehiclename=vehicle.vehiclename";
+            
+        }
+        
+        let sql = `SELECT ${singular} as first, ${sqlMap[q] || q} as second, Count(${sqlMap[q] || q}) as howMany FROM ${table} GROUP  BY ${singular}, ${sqlMap[q] || q} ORDER  BY ${singular};`
+        
+//        console.log(sql);
         
         db.all(sql, [], (err, rows) => {
             if (err) {
@@ -39,21 +63,25 @@ http.createServer(function(request, response) {
             var dict = {};
 
             rows.forEach((row) => {
-                let type = String(row["TYPE"]);
+                let first = String(row["first"]);
                 let howMany = row["howMany"];
-                let decade = row["decade"];
+                let second = row["second"];
 
-//                console.log(row);
-                let arr = type.split("/");
-                arr.forEach((t) => {
-                    const k = [t, decade];
-                    if(dict[k]) {
-                        dict[k] = dict[k] + howMany
-                    } else {
-                        dict[k] = howMany
-                    }
-        //            console.log(t + " [" + howMany + "] " + decade);
-                });
+                if(howMany > 1) {
+                    let arr = first.split("/");
+                    arr.forEach((f) => {
+                        let sArr = second.split("/");
+                        sArr.forEach( (s) => {
+                            const k = [f, s];
+                            if(dict[k]) {
+                                dict[k] = dict[k] + howMany
+                            } else {
+                                dict[k] = howMany
+                            }
+                //            console.log(t + " [" + howMany + "] " + decade);
+                        });
+                    });
+                }
             });
             for (var key in dict) {
                 value = dict[key];
